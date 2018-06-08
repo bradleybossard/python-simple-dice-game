34e5e42f336ce026ec8cc5c5afeeba4c11357f4e
Bradley Bossard <bradleybossard@gmail.com>
2018-06-07 17:57:00 -0700

    Add background image to game

--- game.py
+++ game.py
@@ -9,6 +9,11 @@ Objects
 '''
 Setup
 '''
+
+BLUE  = (25,25,200)
+BLACK = (23,23,23 )
+WHITE = (254,254,254)
+
 worldx = 960
 worldy = 720
 
@@ -17,6 +22,30 @@ ani = 4    # animation rate
 clock = pygame.time.Clock()
 pygame.init()
 
+world    = pygame.display.set_mode([worldx,worldy])
+backdrop = pygame.image.load(os.path.join('images','background.jpg')).convert()
+backdropbox = world.get_rect()
+
+world = pygame.display.set_mode([worldx,worldy])
+
 '''
 Main Loop
 '''
+main = True
+
+while main == True:
+    world.blit(backdrop, backdropbox)
+    # world.fill(BLUE)
+    pygame.display.flip()
+    clock.tick(fps)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit();
+            sys.exit()
+            main = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord('q'):
+                pygame.quit()
+                sys.exit()
+                main = False
